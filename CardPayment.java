6262106f482a715eb3c8f3cf424d346dc9b2e178
susanrtyh <sausanirtiyahh@gmail.com>
2025-11-24 13:44:04 +0700

    CardPayment.java

--- CardPayment.java
+++ CardPayment.java
@@ -1,1 +1,18 @@
+public class CardPayment implements Pembayaran {
+  private int idPembayaran;
 
+  public CardPayment(int idPembayaran) {
+    this.idPembayaran = idPembayaran;
+  }
+
+  @override
+  public int getIdPembayaran() {
+    return idPembayaran;
+  }
+
+  @override
+  public void setIdPembayaran(int id) {
+    this.idPembayaran = id;
+  }
+
+}
